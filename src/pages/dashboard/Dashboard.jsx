ca8fb491e3e3b3eda50601a4cc30214f4e42424d
nayem islam <gangsn490@gmail.com>
2023-08-23 19:51:53 +0600

    some adding

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Sidebar from '../../layout/SideNavStyle';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
 };
 
 export default Dashboard;
