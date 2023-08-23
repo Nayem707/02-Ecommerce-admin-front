7772e2b96dedc6c34554a77156a851de75734f24
nayem islam <gangsn490@gmail.com>
2023-08-23 20:55:39 +0600

    Add dependency chart and setup

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import Sidebar from '../../layout/SideNavStyle';
+import HeaderDash from '../../components/dash/HeaderDash';
+import LineChart from '../../components/common/LineChart';
 
 const Dashboard = () => {
   return (
-    <div>
-      <Sidebar />
-    </div>
+    <>
+      <div className='bg-white relative '>
+        <div className=' col-span-2 flex '>
+          <div className='p-2 pt-10 w-80 h-screen '>
+            <Sidebar />
+          </div>
+          <div className='w-full pt-10 px-4 '>
+            <div className='text-3xl mb-6'>Products Over View</div>
+            <HeaderDash />
+            <div className=''>
+              <LineChart />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
