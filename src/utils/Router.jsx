d749d3f2724a3200bbee2886c0064bcf5e5d96da
nayem islam <gangsn490@gmail.com>
2023-08-23 14:48:19 +0600

    router setup

--- src/utils/Router.jsx
+++ src/utils/Router.jsx
@@ -9,6 +9,10 @@ import HomeView from '../pages/home/HomeView';
 import LoginView from '../pages/login/LoginView';
 import RegisterView from '../pages/register/RegisterView';
 import ErrorView from '../pages/error/ErrorView';
+import Products from '../pages/Products/Products';
+import CartView from '../pages/cart/CartView';
+import SettingView from '../pages/setting/SettingView';
+import ProductDetails from '../components/common/ProductDetails';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,6 +20,11 @@ export const router = createBrowserRouter(
       <Route index={true} element={<HomeView />} />
       <Route path='/login' element={<LoginView />} />
       <Route path='/register' element={<RegisterView />} />
+      <Route path='/products' element={<Products />} />
+      <Route path='/products/:id' element={<ProductDetails />} />
+      <Route path='/cart' element={<CartView />} />
+      <Route path='/setting/user' element={<SettingView />} />
+
       <Route path='*' element={<ErrorView />} />
     </Route>
   )
