e7317f7c03c9b5f44603cae7b95571ec8c54f9ab
nayem islam <gangsn490@gmail.com>
2023-08-23 14:49:38 +0600

    layout setup

--- src/layout/Index.jsx
+++ src/layout/Index.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavStyle from './NavStyle';
+import Topnav from './Topnav';
+import FooterStyle from './FooterStyle';
 
 const Layout = () => {
-  return <div className='dark:text-white '>Layout</div>;
+  return (
+    <>
+      <Topnav />
+      <NavStyle />
+      <Outlet />
+      <FooterStyle />
+    </>
+  );
 };
 
 export default Layout;
